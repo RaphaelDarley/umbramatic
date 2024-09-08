ad318cd63a97397101b18e5386ad1e7eb5d74fc0
Raphael Darley <raphael@raphaeldarley.com>
2024-09-08 01:51:56 +0100

    fix

--- src/arc.rs
+++ src/arc.rs
@@ -1,9 +1,17 @@
-use core::str;
-use std::{mem::transmute, ptr, sync::Arc};
+use core::{fmt, str};
+use std::{
+    fmt::{Debug, Display},
+    hash::Hash,
+    mem::{transmute, ManuallyDrop},
+    ops::Deref,
+    ptr,
+    sync::Arc,
+};
 
 pub const MAX_INLINE: usize = 12;
 
 /// An owned Atomically reference counted Umbra-style string
+#[repr(C)]
 pub struct UmbraArcString {
     len: u32,
     prefix: [u8; 4],
@@ -12,7 +20,7 @@ pub struct UmbraArcString {
 
 pub union UmbraArcExtra {
     data: [u8; 8],
-    ptr: *const str,
+    ptr: *const u8,
 }
 
 impl UmbraArcString {
@@ -23,10 +31,13 @@ impl UmbraArcString {
         let len = val_str.len();
 
         if len <= MAX_INLINE {
+            eprintln!("inlining!!!");
             let mut inline: [u8; 12] = [0; 12];
             inline[..len].copy_from_slice(val_str.as_bytes());
             // SAFETY: inline is of length 12 and align 1, and it is being split into arrays of length 4 and 8
-            let (prefix, extra) = unsafe { transmute(inline) };
+            let (prefix, extra): ([u8; 4], [u8; 8]) = unsafe { transmute(inline) };
+
+            eprintln!("extra: {}", String::from_utf8(extra.to_vec()).unwrap());
 
             UmbraArcString {
                 len: len as u32,
@@ -36,13 +47,11 @@ impl UmbraArcString {
         } else {
             let mut prefix = [0; 4];
             prefix.copy_from_slice(&val_str.as_bytes()[0..4]);
-            let stored: Arc<str> = Arc::from(val_str);
-            let ptr = Arc::into_raw(stored);
 
             UmbraArcString {
                 len: len as u32,
                 prefix,
-                extra: UmbraArcExtra { ptr },
+                extra: UmbraArcExtra::inner_ptr_new(val_str),
             }
         }
     }
@@ -64,8 +73,11 @@ impl UmbraArcString {
             unsafe { &self.extra.data }
         } else {
             // SAFETY: is_inline() so ptr is valid
-            let s = unsafe { &*self.extra.ptr };
-            &s.as_bytes()[4..]
+            let s = unsafe { &*self.extra.inner_ptr_to_arc(self.len) };
+            let tmp_slice = &s.as_bytes()[4..];
+
+            // SAFETY: data is valid for as long as UmbraArcString is
+            unsafe { transmute(tmp_slice) }
         }
     }
 }
@@ -92,15 +104,43 @@ impl Clone for UmbraArcString {
 
 impl AsRef<str> for UmbraArcString {
     fn as_ref(&self) -> &str {
+        &**self
+    }
+}
+
+impl Deref for UmbraArcString {
+    type Target = str;
+
+    fn deref(&self) -> &Self::Target {
         if self.is_inline() {
             // SAFETY: following 8 bytes are extra and data is active as is_inline()
             let byte_arr: &[u8; 12] = unsafe { transmute(&self.prefix) };
             // SAFETY: bytes were taken from str::as_bytes, so should be valid utf-8
-            unsafe { str::from_utf8_unchecked(byte_arr) }
+            unsafe { str::from_utf8_unchecked(&byte_arr[..self.len as usize]) }
         } else {
             // SAFETY: !is_inline() so ptr is active
-            unsafe { &*self.extra.ptr }
+            let tmp_ref: &str = unsafe { &*self.extra.inner_ptr_to_arc(self.len) };
+            // SAFETY: this memory is valid as long as the UmbraArcString is valid
+            unsafe { transmute(tmp_ref) }
         }
+    }
+}
+
+impl Display for UmbraArcString {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        fmt::Display::fmt(&**self, f)
+    }
+}
+
+impl Debug for UmbraArcString {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        fmt::Debug::fmt(&**self, f)
+    }
+}
+
+impl Hash for UmbraArcString {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        (**self).hash(state)
     }
 }
 
@@ -171,6 +211,26 @@ impl Drop for UmbraArcString {
 }
 
 impl UmbraArcExtra {
+    fn inner_ptr_new(val: &str) -> Self {
+        let stored: Arc<str> = Arc::from(val);
+        let str_ptr = Arc::into_raw(stored);
+        let byte_slice = (unsafe { &*str_ptr }).as_bytes();
+        let ptr = byte_slice.as_ptr();
+        Self { ptr }
+    }
+
+    /// SAFETY: Must be called with ptr field and with the value returned from inner_ptr_new, and with the length of the string it was called with
+    unsafe fn inner_ptr_to_arc(&self, len: u32) -> ManuallyDrop<Arc<str>> {
+        // SAFETY: ptr must be active under preconditions
+        let ptr = self.ptr;
+        let byte_slice = ptr::slice_from_raw_parts(ptr, len as usize);
+        // SAFETY: same ptr and length
+        let str_ptr = unsafe { str::from_utf8_unchecked(&*byte_slice) };
+        let str_arc = unsafe { Arc::from_raw(str_ptr) };
+
+        ManuallyDrop::new(str_arc)
+    }
+
     /// SAFETY: Must be called with ptr field active and it containing a pointer from Arc::into_raw
     unsafe fn inner_ptr_clone(&self) -> Self {
         // SAFETY: ptr must be active under preconditions
@@ -203,5 +263,29 @@ impl UmbraArcExtra {
 
         // SAFETY: ptr must have a pointer from Arc::into_raw
         let _ = unsafe { Arc::from_raw(arc_raw) };
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::UmbraArcString;
+
+    #[test]
+    fn basic_test() {
+        let inlinable = "abcdefghijkl";
+        let umbra = UmbraArcString::new(inlinable);
+
+        // eprintln!("{umbra}");
+
+        assert_eq!(umbra.len(), 12);
+
+        assert_eq!(umbra, inlinable)
+    }
+    #[test]
+    fn overflow_test() {
+        let overflow = "abcdefghijklmnopqr";
+        let umbra = UmbraArcString::new(overflow);
+
+        assert_eq!(umbra, overflow)
     }
 }
